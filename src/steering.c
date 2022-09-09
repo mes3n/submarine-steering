4d44a09fc86a17f620d767f78c869184da3e17d0
mes3n <skidmackan@gmail.com>
2022-09-10 00:36:37 +0200

    added 'steering' function and init

--- src/steering.c
+++ src/steering.c
@@ -2,16 +2,17 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 void steering_start (void) {
-
+    memset(&movement, 0, sizeof(movement));
 }
 
-void steer (steering_t movement) {
+void steer (void) {
     while (!steering_should_stop) {
-        printf("%2f\n", movement.speed);
-        printf("%2f, %2f\n", movement.angle[0], movement.angle[1]);
-        sleep(2);
+        printf("speed: %.2f\n", movement.speed);
+        printf("%.2f, %.2f\n", movement.angle[0], movement.angle[1]);
+        sleep(1);
     }
 }
 
